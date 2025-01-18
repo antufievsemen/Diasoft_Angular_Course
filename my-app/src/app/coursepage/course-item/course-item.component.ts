0f139f99ca293ac2c0e65aa413979d1a0fc095ba
semen.antufiev <alexantufiev@gmail.com>
2025-01-18 11:52:27 +0300

    lesson_2 finished

---
+++ my-app/src/app/coursepage/course-item/course-item.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Course } from 'src/app/domain/course';
+
+@Component({
+  selector: 'app-course-item',
+  templateUrl: './course-item.component.html',
+  styleUrls: ['./course-item.component.scss']
+})
+export class CourseItemComponent {
+  @Input()
+  public course: Course = {} as Course;
+  @Output()
+  public deleteCourse: EventEmitter<Course> = new EventEmitter<Course>();
+  @Output()
+  public editCourse: EventEmitter<Course> = new EventEmitter<Course>();
+
+  public delete(): void {
+    console.log('Delete ' + this.course.id);
+  }
+
+  public edit(): void {
+    console.log('Edit ' + this.course.id);
+  }
+}
